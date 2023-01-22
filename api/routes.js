9ca38dc6e5cebb54809fbf03601b4a167aee59a3
Abrham Alemayehu <aaabrham421@gmail.com>
2023-01-23 01:08:49 +0300

    changed routes folder to api

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const boardsRouter = require("../routes/boards.route");
-const tasksRouter = require("../routes/tasks.route");
-const usersRouter = require("../routes/users.route");
+const boardsRouter = require("./boards.route");
+const tasksRouter = require("./tasks.route");
+const usersRouter = require("./users.route");
 
 router.use("/boards", boardsRouter);
 router.use("/tasks", tasksRouter);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const createError = require("http-errors");
-const routes = require("./routes/routes");
+const routes = require("./api/routes");
 const cors = require("cors");
 const { User } = require("./models/user.model");
 const { Board } = require("./models/board.model");

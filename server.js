513b735655c63a89742d59538c6002ae002ac464
Abrham Alemayehu <aaabrham421@gmail.com>
2023-01-23 10:18:34 +0300

    returned database connection

--- server.js
+++ server.js
@@ -10,38 +10,38 @@ const { Task } = require("./models/task.model");
 const { local_uri } = require("./configs/db.config");
 
 //db connection
-// module.exports = mongoose
-//   .connect(process.env.ATLAS_URI || local_uri)
-//   .then((result) => {
-// console.log("Successfully Connected to Database!");
+module.exports = mongoose
+  .connect(process.env.ATLAS_URI || local_uri)
+  .then((result) => {
+    console.log("Successfully Connected to Database!");
 
-const app = express();
+    const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use("/api", (req, res) => {
-  res.send("works");
-});
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cors());
+    app.use("/api", (req, res) => {
+      res.send("works");
+    });
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      next(createError(404));
+    });
 
-// error handler middleware
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 5000;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({ success: false, message: err.message });
+    // error handler middleware
+    app.use((err, req, res, next) => {
+      const statusCode = err.statusCode || 5000;
+      console.error(err.message, err.stack);
+      res.status(statusCode).json({ success: false, message: err.message });
 
-  return;
-});
+      return;
+    });
 
-const port = process.env.PORT || 5000;
+    const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server Listening on port ${port}`));
-// })
-// .catch((err) => {
-//   console.log("MongoDB Connection Error:", err);
-// });
+    app.listen(port, () => console.log(`Server Listening on port ${port}`));
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error:", err);
+  });

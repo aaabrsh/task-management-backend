1494937cd484a09afa51a8e01c2e745e54be05cd
Abrham Alemayehu <aaabrham421@gmail.com>
2023-01-23 10:15:55 +0300

    fix on server .js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/api", () => {
+app.use("/api", (req, res) => {
   res.send("works");
 });
 
